62381f732dd55847117ce5279c582e5d0565b1d0
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2021-09-23 03:37:51 +0530

    feat: added previous palindrome date function

--- app.js
+++ app.js
@@ -48,6 +48,8 @@ function getAllDates(date) {
 }
 
 function checkPalindromeAllDateFormats(date) {
+    var dateFormats = ['ddmmyyyy', 'mmddyyyy', 'yyyymmdd', 'ddmmyy', 'mmddyy', 'yymmdd'];
+
     var listOfPalindromes = getAllDates(date);
 
     var isPalindrome = false;
@@ -63,12 +65,156 @@ function checkPalindromeAllDateFormats(date) {
     return isPalindrome;
 }
 
+function isLeapYear(year) { 
+    if(year % 400 === 0) {
+        return true;
+    }
 
+    if(year % 100 === 0) {
+        return false;
+    }
 
-var date = {
-    day: 2,
-    month: 11,
-    year: 2020
+    if(year % 4 === 0) {
+        return true;
+    }
+
+    return false;
 }
 
-console.log(checkPalindromeAllDateFormats(date));
+function incrementDate(date) {
+    var day = date.day + 1;
+    var month = date.month;
+    var year = date.year;
+
+    var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if(month === 2) {
+        if(isLeapYear(year)) {
+            if(day > 29) {
+                day = 1;
+                month = month + 1;
+            }
+        } else {
+            if(day > 28) {
+                day = 1;
+                month = month + 1;
+            }
+        }
+
+    }
+    else {
+        if(day > daysInMonth[month - 1]) {
+            day = 1;
+            month = month + 1;
+        }
+    }
+
+    if(month > 12) {
+        month = 1;
+        year = year + 1;
+    }
+
+    return {
+        day: day,
+        month: month,
+        year: year
+    };
+
+}
+
+function getNextPalindromeDate(date) {
+    var countDays = 0;
+    var nextDate = incrementDate(date);
+
+    while(1) {
+        countDays = countDays + 1;
+        var isPal = checkPalindromeAllDateFormats(nextDate);
+
+        if(isPal) {
+            break;
+        }
+
+        nextDate = incrementDate(nextDate);
+    }
+
+    return [countDays, nextDate];
+}
+
+function decrementDate(date) {
+    var day = date.day - 1;
+    var month = date.month;
+    var year = date.year;
+
+    var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if(month === 3) {
+        if(isLeapYear(year)) {
+            if(day < 1) {
+                day = 29;
+                month = month - 1;
+            }
+        } else {
+            if(day < 1) {
+                day = 28;
+                month = month - 1;
+            }
+        }
+
+    }
+
+    if (day < 1) {
+        month = month - 1; 
+        if (month < 1) {
+            year = year - 1;
+            month = 12;
+            day = daysInMonth[month - 1];
+        } else {
+            day = daysInMonth[month - 1];
+        }
+    }
+
+    return {
+        day: day,
+        month: month,
+        year: year
+    };
+
+}
+
+function getPrevPalindromeDate(date) {
+    var countDaysPrev = 0;
+    var prevDate = decrementDate(date);
+
+    while(1) {
+        countDaysPrev = countDaysPrev + 1;
+        var isPal = checkPalindromeAllDateFormats(prevDate);
+
+        if(isPal) {
+            break;
+        }
+
+        prevDate = decrementDate(prevDate);
+    }
+
+    return [countDaysPrev, prevDate];
+}
+
+function getNearestPalindrome(date) {
+    const prev = getPrevPalindromeDate(date);
+    const next = getNextPalindromeDate(date);
+
+    if(prev[0] < next[0]) {
+        return prev;
+    } else {
+        return next;
+    }
+
+}
+
+var date = {
+    day: 12,
+    month: 2,
+    year: 2020
+};
+
+console.log(getNearestPalindrome(date));
